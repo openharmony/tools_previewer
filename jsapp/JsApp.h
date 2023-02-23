f5c831a1e93bbb212683342b45ffc7d3c309df34
libenyao <libenyao@h-partners.com>
2023-02-23 16:26:35 +0800

    add jsapp/BUILD.gn CMakeList.txt JsApp.cpp JsApp.h and jsapp/lite
    Signed-off-by: libenyao <libenyao@h-partners.com>

---
+++ jsapp/JsApp.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef JSAPP_H
+#define JSAPP_H
+
+#include <atomic>
+#include <string>
+#include <thread>
+#include <vector>
+#include "json.h"
+
+class JsApp {
+public:
+    JsApp& operator=(const JsApp&) = delete;
+    JsApp(const JsApp&) = delete;
+    virtual void Start() = 0;
+    virtual void Restart() = 0;
+    virtual void Interrupt() = 0;
+    void Stop();
+    void SetJsAppPath(const std::string& value);
+    void SetUrlPath(const std::string& value);
+    void SetPipeName(const std::string& name);
+    void SetPipePort(const std::string& port);
+    void SetBundleName(const std::string& name);
+    void SetRunning(bool flag);
+    bool GetRunning();
+    void SetIsDebug(bool value);
+    void SetDebugServerPort(uint16_t value);
+    void SetJSHeapSize(uint32_t size);
+    virtual std::string GetJSONTree();
+    virtual std::string GetDefaultJSONTree();
+    virtual void OrientationChanged(std::string);
+    virtual void ResolutionChanged(int32_t, int32_t, int32_t, int32_t, int32_t);
+    virtual void SetArgsColorMode(const std::string& name);
+    virtual void SetArgsAceVersion(const std::string& aceVersion);
+    virtual std::string GetOrientation() const;
+    virtual std::string GetColorMode() const;
+    virtual void ColorModeChanged(const std::string);
+    static bool IsLiteDevice(std::string);
+    virtual void ReloadRuntimePage(const std::string);
+    virtual void SetScreenDensity(const std::string);
+    virtual void SetConfigChanges(const std::string);
+    virtual bool MemoryRefresh(const std::string) const;
+    virtual void LoadDocument(const std::string, const std::string, const Json::Value);
+
+protected:
+    JsApp();
+    virtual ~JsApp(){};
+    std::string pipeName;
+    std::string pipePort;
+    std::string jsAppPath;
+    std::string bundleName;
+    std::string urlPath;
+    std::atomic<bool> isFinished;
+    std::atomic<bool> isRunning;
+    bool isDebug;
+    uint16_t debugServerPort;
+    uint32_t jsHeapSize;
+    std::string colorMode;
+    std::string orientation;
+    std::string aceVersion;
+    std::string screenDensity;
+    std::string configChanges;
+};
+static std::vector<std::string> liteDevice = {"liteWearable", "smartVision"};
+
+#endif // JSAPP_H
