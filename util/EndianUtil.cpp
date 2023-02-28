3801326131bc1897cb0d406ab99fcfec5f0e0b9f
libenyao <libenyao@h-partners.com>
2023-02-28 14:54:09 +0800

    add util part 1
    Signed-off-by: libenyao <libenyao@h-partners.com>

---
+++ util/EndianUtil.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "EndianUtil.h"
+
+bool EndianUtil::IsBigEndian()
+{
+    union {
+        uint32_t data;
+        char buffer[sizeof(uint32_t)];
+    } test = {ENDIAN_TEST_MAGIC};
+
+    return test.buffer[0] == BIG_ENDIAN_FIRST_BYTE;
+}
