8965ad1de88e0ede69ccd099c2dff70fe07057d1
sambaby <1337922982@qq.com>
2023-09-13 14:42:46 +0800

    fixed 425180b from https://gitee.com/sambaby/tools_previewer/pulls/137 fix problem context undefined
    Signed-off-by: sambaby <1337922982@qq.com>

---
+++ automock/mock-generate/src/generate/generateExportFunction.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { SourceFile } from 'typescript';
+import { SyntaxKind } from 'typescript';
+import type { FunctionEntity } from '../declaration-node/functionDeclaration';
+import { getCallbackStatement, getReturnStatement } from './generateCommonUtil';
+
+/**
+ * generate function
+ * @param rootName
+ * @param functionArray
+ * @param sourceFile
+ * @param mockApi
+ * @returns
+ */
+export function generateExportFunction(functionEntity: FunctionEntity, sourceFile: SourceFile, mockApi: string): string {
+  let functionBody = '';
+  functionBody = `global.${functionEntity.functionName} = function (...args) {`;
+  functionBody += `console.warn('The ${functionEntity.functionName} interface in the Previewer is a mocked implementation and may behave differently than on a real device.');\n`;
+
+  const args = functionEntity.args;
+  const len = args.length;
+  if (args.length > 0 && args[len - 1].paramName.toLowerCase().includes('callback')) {
+    functionBody += getCallbackStatement(mockApi);
+  }
+  if (functionEntity.returnType.returnKind !== SyntaxKind.VoidKeyword) {
+      functionBody += getReturnStatement(functionEntity.returnType, sourceFile);
+  }
+  functionBody += '}';
+  return functionBody;
+}
